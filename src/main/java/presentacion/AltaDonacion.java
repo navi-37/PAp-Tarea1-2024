31b064ae0dad0c87f06e436c242aa4ce017511ad
Ivan <ivan10czr@gmail.com>
2024-08-23 20:44:40 -0300

    update alta donacion, faltan algunas validaciones

--- src/main/java/presentacion/AltaDonacion.java
+++ src/main/java/presentacion/AltaDonacion.java
@@ -10,6 +10,7 @@ import javax.swing.JToggleButton;
 import datatypes.DtAlimento;
 import datatypes.DtArticulo;
 import datatypes.DtDonacion;
+import excepciones.DonacionRepetidaExc;
 import interfaces.IControlador;
 
 //import excepciones.SocioRepetidoExcepcion;
@@ -20,6 +21,7 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
@@ -30,7 +32,6 @@ public class AltaDonacion extends JInternalFrame {
 	
 	private IControlador icon;
 	private JTextField textid;
-	private JTextField textFechaIngreso;
 	private JTextField textDimensiones;
 	private JTextField textPeso;
 	private JTextField textCantElem;
@@ -43,13 +44,9 @@ public class AltaDonacion extends JInternalFrame {
 		getContentPane().setLayout(null);
 		
 		textid = new JTextField();
-		textid.setBounds(56, 39, 86, 20);
+		textid.setBounds(56, 33, 60, 20);
 		getContentPane().add(textid);
 		textid.setColumns(10);
-		
-		textFechaIngreso = new JTextField();
-		textFechaIngreso.setBounds(162, 76, 95, 20);
-		getContentPane().add(textFechaIngreso);
 		
 		//JComboBox<String> 
 		comboBoxTipoDonacion = new JComboBox<String>();
@@ -71,33 +68,30 @@ public class AltaDonacion extends JInternalFrame {
 				
 			}
 		});
+		
 		comboBoxTipoDonacion.setModel(new DefaultComboBoxModel<String>(new String[] {"Alimento", "Artículo"}));
 		comboBoxTipoDonacion.setToolTipText("");
-		comboBoxTipoDonacion.setBounds(162, 125, 95, 22);
+		comboBoxTipoDonacion.setBounds(160, 80, 95, 22);
 		getContentPane().add(comboBoxTipoDonacion);
 		
-		JLabel lblFechaDeIngreso = new JLabel("Fecha de ingreso");
-		lblFechaDeIngreso.setBounds(34, 71, 131, 28);
-		getContentPane().add(lblFechaDeIngreso);
-		
 		JLabel lblTipoDeDonacin = new JLabel("Tipo de donación");
-		lblTipoDeDonacin.setBounds(34, 126, 134, 22);
+		lblTipoDeDonacin.setBounds(34, 80, 134, 22);
 		getContentPane().add(lblTipoDeDonacin);
 		
 		JLabel lblId = new JLabel("ID");
-		lblId.setBounds(34, 41, 70, 15);
+		lblId.setBounds(34, 35, 70, 15);
 		getContentPane().add(lblId);
 		
 		JLabel lblDescripcin = new JLabel("Descripción");
-		lblDescripcin.setBounds(34, 169, 111, 20);
+		lblDescripcin.setBounds(34, 129, 111, 20);
 		getContentPane().add(lblDescripcin);
 		
 		JLabel lblCantidadDeElementos = new JLabel("Cantidad de elementos");
-		lblCantidadDeElementos.setBounds(34, 216, 189, 15);
+		lblCantidadDeElementos.setBounds(34, 178, 176, 15);
 		getContentPane().add(lblCantidadDeElementos);
 		
 		JLabel lblPeso = new JLabel("Peso");
-		lblPeso.setBounds(34, 243, 70, 15);
+		lblPeso.setBounds(34, 227, 70, 15);
 		getContentPane().add(lblPeso);
 		
 		JLabel lblDimensiones = new JLabel("Dimensiones");
@@ -105,22 +99,22 @@ public class AltaDonacion extends JInternalFrame {
 		getContentPane().add(lblDimensiones);
 		
 		textDimensiones = new JTextField();
-		textDimensiones.setBounds(143, 268, 114, 19);
+		textDimensiones.setBounds(131, 267, 79, 19);
 		getContentPane().add(textDimensiones);
 		textDimensiones.setColumns(10);
 		
 		textPeso = new JTextField();
-		textPeso.setBounds(81, 241, 114, 19);
+		textPeso.setBounds(71, 224, 60, 19);
 		getContentPane().add(textPeso);
 		textPeso.setColumns(10);
 		
 		textCantElem = new JTextField();
-		textCantElem.setBounds(214, 214, 114, 19);
+		textCantElem.setBounds(184, 175, 60, 19);
 		getContentPane().add(textCantElem);
 		textCantElem.setColumns(10);
 		
 		textDescripcion = new JTextField();
-		textDescripcion.setBounds(132, 170, 114, 19);
+		textDescripcion.setBounds(128, 129, 228, 19);
 		getContentPane().add(textDescripcion);
 		textDescripcion.setColumns(10);
 		
@@ -130,7 +124,7 @@ public class AltaDonacion extends JInternalFrame {
 				agregarDonacionAceptarActionPerformed(e);		
 			}
 		});
-		btnAceptar.setBounds(132, 311, 117, 25);
+		btnAceptar.setBounds(128, 319, 117, 25);
 		getContentPane().add(btnAceptar);
 		
 		JButton btnCancelar = new JButton("Cancelar");
@@ -139,31 +133,35 @@ public class AltaDonacion extends JInternalFrame {
 				agregarDonacionCancelarActionPerformed(e);
 			}
 		});
-		
-		btnCancelar.setBounds(302, 311, 117, 25);
+		btnCancelar.setBounds(298, 319, 117, 25);
 		getContentPane().add(btnCancelar);
 
 	}
 	
 	protected void agregarDonacionAceptarActionPerformed(ActionEvent arg0) {
 		Integer id = Integer.valueOf(this.textid.getText());
-        String fecha = this.textFechaIngreso.getText();
+        //String fecha = this.textFechaIngreso.getText();
+		LocalDateTime fecha = LocalDateTime.now();
         String selectedItem = (String) comboBoxTipoDonacion.getSelectedItem();
         DtDonacion dt = null;
 		if (selectedItem.equals("Alimento")) {
 			String descripcionProductos = this.textDescripcion.getText();
 			Integer cantElementos = Integer.valueOf(this.textCantElem.getText());
-			//dt = new DtAlimento(id, fecha, descripcionProductos, cantElementos);
-			//castear fecha a localdatetime o hacer que directamente se ingrese en ese formato
+			dt = new DtAlimento(id, fecha, descripcionProductos, cantElementos);
 		} else if (selectedItem.equals("Artículo")) {
 			String descripcion = this.textDescripcion.getText();
 			float peso = Float.parseFloat(this.textPeso.getText());
 			String dimensiones = this.textDimensiones.getText();
-			//dt = new DtArticulo(id, fecha, descripcion, peso, dimensiones);
-			//castear fecha a localdatetime o hacer que directamente se ingrese en ese formato
+			dt = new DtArticulo(id, fecha, descripcion, peso, dimensiones);
 		}
         //hay que usar la instancia de icon para que agregue el dt
-        
+        try {
+        	this.icon.altaDonacion(dt);
+        	JOptionPane.showMessageDialog(this, "Donación dada de alta con éxito", "Alta donación", JOptionPane.INFORMATION_MESSAGE);
+        	} catch (DonacionRepetidaExc e) {
+            JOptionPane.showMessageDialog(this, e.getMessage(), "Alta donación", JOptionPane.ERROR_MESSAGE);
+        	}
+   
         limpiarFormulario();
         setVisible(false);
 	} 
@@ -175,7 +173,6 @@ public class AltaDonacion extends JInternalFrame {
 	
 	private void limpiarFormulario() {
         textid.setText("");
-        textFechaIngreso.setText("");
         textDimensiones.setText("");
         textPeso.setText("");
         textDescripcion.setText("");
