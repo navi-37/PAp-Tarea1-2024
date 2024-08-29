75b0ff0b6d8b2d1a32ac573157e742fea9b969f8
Manuel Sainz <manuelsainz6@gmail.com>
2024-08-28 23:26:31 -0300

    logic of modificarDstribucion
    :wq#

---
+++ src/main/java/excepciones/DistribucionNoEncontradaExc.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class DistribucionNoEncontradaExc extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public DistribucionNoEncontradaExc(String message) {
+        super(message);
+    }
+}
--- src/main/java/interfaces/IControlador.java
+++ src/main/java/interfaces/IControlador.java
@@ -13,6 +13,8 @@ import datatypes.DtUsuario;
 import datatypes.EstadoDistribucion;
 import excepciones.DonacionRepetidaExc;
 import excepciones.UsuarioRepetidoExc;
+import logica.Distribucion;
+import excepciones.DistribucionNoEncontradaExc;
 import excepciones.DistribucionRepetidaExc;
 import excepciones.DonacionNoExisteExc;
 import excepciones.UsuarioNOBeneficiarioExc;
@@ -25,13 +27,13 @@ public interface IControlador {
 	public void altaUsuario(DtUsuario usuario) throws UsuarioRepetidoExc;
   
 	public void altaDistribucion(DtDistribucion distribucion) throws DistribucionRepetidaExc, UsuarioNOBeneficiarioExc, DonacionNoExisteExc;
-	
 
 	public Integer[] listarLasDistribucionesFiltradas(EstadoDistribucion estado, Barrio zona);
 	
 	public DtDistribucion getDistribucion(int idDist);
 	
-	
 	public ArrayList<DtBeneficiario> ListaBeneficiarios();
+
+	void modificarDistribucion(DtDistribucion distribucion) throws DistribucionNoEncontradaExc;
 
 }
