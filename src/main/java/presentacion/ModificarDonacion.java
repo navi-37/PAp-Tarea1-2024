d03584c958167ac67ab138524a36dd24bf726203
Ivan <ivan10czr@gmail.com>
2024-08-31 18:40:02 -0300

    quedó funcando y con validaciones varias

--- src/main/java/presentacion/ModificarDonacion.java
+++ src/main/java/presentacion/ModificarDonacion.java
@@ -7,6 +7,7 @@ import javax.swing.JInternalFrame;
 import interfaces.IControlador;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import datatypes.DtAlimento;
@@ -105,6 +106,7 @@ public class ModificarDonacion extends JInternalFrame {
 		getContentPane().add(lblTipoDonacion);
 		
 		textFieldTipoDonacion = new JTextField();
+		textFieldTipoDonacion.setEditable(false);
 		textFieldTipoDonacion.setBounds(154, 80, 120, 20);
 		getContentPane().add(textFieldTipoDonacion);
 		textFieldTipoDonacion.setColumns(10);
@@ -150,12 +152,17 @@ public class ModificarDonacion extends JInternalFrame {
 				DtAlimento donacionAlimento = (DtAlimento) donacion;
 				textFieldDescripcion.setText(donacionAlimento.getDescripcionProductos());
 				textFieldCantElem.setText(String.valueOf(donacionAlimento.getCantElementos()));
+				//anulamos los texfields que no corresponden al dtAlimento
+				textFieldPeso.setEnabled(false);
+				textFieldDimensiones.setEnabled(false);
 			} else if (donacion instanceof DtArticulo) {
 				textFieldTipoDonacion.setText("Artículo");
 				DtArticulo donacionArticulo = (DtArticulo) donacion;
 				textFieldDescripcion.setText(donacionArticulo.getDescripcion());
 				textFieldPeso.setText(String.valueOf(donacionArticulo.getPeso()));
 				textFieldDimensiones.setText(donacionArticulo.getDimensiones());
+				//same pero para artículo
+				textFieldCantElem.setEnabled(false);
 			}
 		}
 	}
@@ -174,49 +181,102 @@ public class ModificarDonacion extends JInternalFrame {
 		textFieldCantElem.setText("");
 		textFieldPeso.setText("");
 		textFieldDimensiones.setText("");
+		textFieldCantElem.setEnabled(true);
+	    textFieldPeso.setEnabled(true);
+	    textFieldDimensiones.setEnabled(true);
 	}
+	
 	
 	public void modificarDatos() {
-		//acá habría que llamar un checkformulario que se fije que las cosas esten en orden
-		//
+		if (checkFormulario()) {
 		
-		// id y fecha no se ven afectados por el tipo de donación
-		Integer id = (Integer) comboBoxDonaciones.getSelectedItem();
-		
-		DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-		LocalDateTime nuevaFechaHoraIngreso = null;
-		try {
-	        nuevaFechaHoraIngreso = LocalDateTime.parse(textFieldFechaIng.getText(), formatoFecha);
-	    } catch (DateTimeParseException e) {
-	        // si el formato del string no es válido
-	        e.printStackTrace();
-	        // Mostrar mensaje de error o manejar según la lógica de tu aplicación
-	        return;
-	    }
-		
-		//descripción tienen ambos aunque con distinto nombre, acá se pueden compartir
-		String nuevaDescripcion = textFieldDescripcion.getText();
-		
-		String tipoDonacion = textFieldTipoDonacion.getText();
-		
-		if (tipoDonacion.equals("Alimento")) {
-		    Integer nuevaCantElem = null;
-		    if (!textFieldCantElem.getText().isEmpty()) {
-		        nuevaCantElem = Integer.parseInt(textFieldCantElem.getText());
+			// id y fecha no se ven afectados por el tipo de donación
+			Integer id = (Integer) comboBoxDonaciones.getSelectedItem();
+			
+			DateTimeFormatter formatoFecha = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+			LocalDateTime nuevaFechaHoraIngreso = null;
+			try {
+		        nuevaFechaHoraIngreso = LocalDateTime.parse(textFieldFechaIng.getText(), formatoFecha);
+		    } catch (DateTimeParseException e) {
+		        JOptionPane.showMessageDialog(this, "La fecha debe ser ingresada en formato dd/MM/yyyy HH:mm:ss", "Modificar donación",
+	                    JOptionPane.ERROR_MESSAGE);
+		        return;
 		    }
-			DtAlimento donacion = new DtAlimento(id, nuevaFechaHoraIngreso, nuevaDescripcion, nuevaCantElem);
-			this.icon.modificarDonacion(donacion);
-		    
-		} else if (tipoDonacion.equals("Artículo")) {
-			Float nuevoPeso = null;
-		    if (!textFieldPeso.getText().isEmpty()) {
-		        nuevoPeso = Float.parseFloat(textFieldPeso.getText());
-		    }
-			String nuevasDimensiones = textFieldDimensiones.getText();
-			DtArticulo donacion = new DtArticulo(id, nuevaFechaHoraIngreso, nuevaDescripcion, nuevoPeso, nuevasDimensiones);
-			this.icon.modificarDonacion(donacion);
-		}	
+			
+			//descripción tienen ambos aunque con distinto nombre, acá se pueden compartir
+			String nuevaDescripcion = textFieldDescripcion.getText();
+			if (nuevaDescripcion == null || nuevaDescripcion.trim().isEmpty()) {
+	            JOptionPane.showMessageDialog(this, "La descripción no puede estar vacía", "Modificar donación",
+	                    JOptionPane.ERROR_MESSAGE);
+	            return;
+	        }
+			
+			String tipoDonacion = textFieldTipoDonacion.getText();
+			
+			if (tipoDonacion.equals("Alimento")) {
+			    Integer nuevaCantElem = null;
+			    try {
+				    if (!textFieldCantElem.getText().isEmpty()) {
+				        nuevaCantElem = Integer.parseInt(textFieldCantElem.getText());
+				        if (nuevaCantElem < 0) {
+	                        throw new NumberFormatException("La cantidad de elementos no puede ser negativa.");
+	                    }
+				    }
+			    } catch (NumberFormatException e) {
+			    	 JOptionPane.showMessageDialog(this, "La cantidad de elementos debe ser un número natural", "Modificar donación",
+		                        JOptionPane.ERROR_MESSAGE);
+		                return;
+			    }
+			    
+				DtAlimento donacion = new DtAlimento(id, nuevaFechaHoraIngreso, nuevaDescripcion, nuevaCantElem);
+				this.icon.modificarDonacion(donacion);
+				JOptionPane.showMessageDialog(this, "Modificación realizada con éxito", "Modificar donación",
+                        JOptionPane.INFORMATION_MESSAGE);
+				limpiarFormulario(); 
+				actualizarDonaciones(); //para que la combobox deje de mostrar el id luego de haber modificado
+			    
+			} else if (tipoDonacion.equals("Artículo")) {
+				Float nuevoPeso = null;
+				try {
+				    if (!textFieldPeso.getText().isEmpty()) {
+				        nuevoPeso = Float.parseFloat(textFieldPeso.getText());
+				        if (nuevoPeso < 0) {
+	                        throw new NumberFormatException("El peso no puede ser negativo.");
+	                    }
+	                }
+				} catch (NumberFormatException e) {
+	                JOptionPane.showMessageDialog(this, "El peso debe ser un número decimal positivo", "Modificar donación",
+	                        JOptionPane.ERROR_MESSAGE);
+	                return;
+	            }
+				
+				String nuevasDimensiones = textFieldDimensiones.getText();
+				if (nuevasDimensiones == null || nuevasDimensiones.trim().isEmpty()) {
+	                JOptionPane.showMessageDialog(this, "Las dimensiones no pueden estar vacías", "Modificar donación",
+	                        JOptionPane.ERROR_MESSAGE);
+	                return;
+	            }
+				
+				DtArticulo donacion = new DtArticulo(id, nuevaFechaHoraIngreso, nuevaDescripcion, nuevoPeso, nuevasDimensiones);
+				this.icon.modificarDonacion(donacion);
+				JOptionPane.showMessageDialog(this, "Modificación realizada con éxito", "Modificar donación",
+                        JOptionPane.INFORMATION_MESSAGE);
+				limpiarFormulario();
+				actualizarDonaciones(); //para que la combobox deje de mostrar el id luego de haber modificado
+			}
+		}
 	
 	}
+
+
+	private boolean checkFormulario() {
+	    if (this.comboBoxDonaciones.getSelectedItem() == null) {
+	        JOptionPane.showMessageDialog(this, "Se debe seleccionar una donación", "Modificar donación",
+	                JOptionPane.ERROR_MESSAGE);
+	        return false;
+	    }
+	    return true;
+	}
+
 	
 }
